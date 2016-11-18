f6523f367ef038e8087a4fb030ec01c7e3818deb
Jim <tfa1979@gmail.com>
2016-11-18 15:30:48 -0600

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 lock '3.6.1'
 
 set :application, 'alt_test'
-set :repo_url, 'https://github.com/notch8/alt_test.git'
+set :repo_url, 'https://github.com/butlerjim/alt_test.git'
 set :rbenv_type, :system
 set :rbenv_ruby, 'ruby 2.3.0p0'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/opt/atla'
+set :deploy_to, '/opt/atla-hydra'
 set :passenger_restart_with_touch, true
 # Default value for :scm is :git
 # set :scm, :git
